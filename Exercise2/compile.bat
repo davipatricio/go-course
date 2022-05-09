74e6355894a73c8438c940cadbf5d31b27744136
davipatricio <davipatricio2006@gmail.com>
2022-05-09 19:18:25 -0300

    Add Exercise #2

---
+++ Exercise2/compile.bat
@@ -0,0 +1,2 @@
+go fmt
+go build -ldflags="-s -w" -o Exercise2.exe main.go
---
+++ Exercise2/main.go
@@ -0,0 +1,13 @@
+// Ref: https://www.youtube.com/watch?v=Q7ZrIDMj9zc
+
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x, y, z)
+}
